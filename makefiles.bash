6616548d7c7a4e59de3c29e413d333a2feb160a9
root <root@ip-72-167-50-223.ip.secureserver.net>
2022-12-08 19:58:35 +0000

    msh para crear carpetas del postgres

---
+++ makefiles.bash
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd data/db
+mkdir pg_tblspc
+mkdir pg_dynshmem
+mkdir pg_logical/mappings
+mkdir pg_logical/snapshots
+mkdir pg_notify
+mkdir pg_replslot
+mkdir pg_serial
+mkdir pg_snapshots
+mkdir pg_stat
+mkdir pg_tblspc
+mkdir pg_twophase
+mkdir pg_wal/archive_status
