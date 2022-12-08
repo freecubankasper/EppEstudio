99afad5b93663f77955e4ec0c1011f29e30dfbac
root <root@ip-72-167-50-223.ip.secureserver.net>
2022-12-08 17:47:57 +0000

    mMantener carpetas vacias

--- runserver.sh
+++ runserver.sh
@@ -6,5 +6,5 @@ cd /eppEstudio50
 echo "Collecting Statics Files..."
 python manage.py collectstatic --noinput   
 echo "Starting server web..."
-#python manage.py runserver 0.0.0.0:8000
-gunicorn epp.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
+#gunicorn epp.wsgi:application --bind 0.0.0.0:8000
